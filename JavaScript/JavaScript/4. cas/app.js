07601308a8f4db59c3b3918b03bae4387a75e6fb
asijasijaricc <asijasijaricc12@gmail.com>
2023-03-29 23:00:35 +0200

    kondicionali

---
+++ JavaScript/JavaScript/4. cas/app.js
@@ -0,0 +1,35 @@
+// Kondicionali:
+
+if ("prvi uslov") {
+  // blok koda za izvrsavanje u slucaju da je zadovoljen prvi uslov
+} else if ("drugi uslov") {
+  // blok koda za izvrsavanje u slucaju da je zadovoljen drugi uslov
+} else {
+  // blok koda koji ce se izvrsvati u slucaju da sve prethodno nije zadovoljeno
+}
+
+// 1. Zadatak
+// Ako je broj godine >= 18,
+// neka se ispise poruka "punoletni ste", inace nek se ispise poruka maloletni ste;
+
+const brojGodina = 26;
+
+if (brojGodina >= 18) {
+  console.log("punoletni ste");
+} else {
+  console.log("maloletni ste");
+}
+
+// 2. Zadatak
+// Na osnovu dve promenljive x i y napisati program koji izracunava i stampa kolicnik x/y, sko je broj razlicit od nule,
+// a inace ispunjuje poruku: Deljenje je nemoguce.
+
+let x = 16;
+let y = 2;
+// let y = 0;
+
+if (y === 0) {
+  console.log("Deljenje je nemoguce.");
+} else {
+  console.log(x / y);
+}
