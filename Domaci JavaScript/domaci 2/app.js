7c6cd271eab8de7b73f7bbe053cbd374333c7afe
asijasijaricc <asijasijaricc12@gmail.com>
2023-04-02 00:11:25 +0200

    domaci

---
+++ Domaci JavaScript/domaci 2/app.js
@@ -0,0 +1,10 @@
+// Domaci zadaci:
+// 1. if
+// Napisati program koji na osnovu dva uneta broja od strane korisnika ispisuje:
+// Povrsinu kvadrata ako su uneti brojevi jednaki,
+// Povrsinu pravougaonika ako su uneti brojevi razliciti.
+
+// 2. Zadatak - switch naredba
+// Korisnik unosi dva realna broja x i y. Napisati program koji izracunava i stampa
+// kolicnik x/y, ako je broj y razlicit od nule,
+// a inace ispisuje poruku: Deljenje je nemoguce.
