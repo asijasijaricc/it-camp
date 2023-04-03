3618e2d87d03e7783ea8432d77686f639eb7c45a
asijasijaricc <asijasijaricc12@gmail.com>
2023-04-03 21:20:03 +0200

    domaci

---
+++ Domaci JavaScript/domaci 3/app.js
@@ -0,0 +1,31 @@
+// Domaci:
+// 1.
+// Ispisati neparne brojeve od 1 do 20 na dva nacina.
+// 2.
+// Ispisati sve brojeve od 50 do 100 koji su deljivi sa 5.
+// 3.
+// Suma neparnih prirodnih brojeva od 10 do 20.
+
+//1.
+
+for (let i = 1; i < 20; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+//2.
+
+for (let i = 50; i < 50; i++) {
+  if (i % 5) {
+    console.log(i);
+  }
+}
+
+//3.
+
+for (let i = 10; i < 20; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
