5290b46b63f3f5eff604f8143201c189e5e5c858
asijasijaricc <asijasijaricc12@gmail.com>
2023-04-12 13:30:06 +0200

    vezba

--- JavaScript/3. cas/app.js
+++ JavaScript/3. cas/app.js
@@ -7,7 +7,8 @@ console.log(x);
 var x = 25;
 console.log(x);
 
-// Za sve promenljive definisane preko var keyword je moguce izvrsiti redeklaraciju (ponovno definisanje promenljive) i reinicijalizaciju (dodeljivanje nove vrednosti).
+// Za sve promenljive definisane preko var keyword je moguce izvrsiti redeklaraciju (ponovno
+// definisanje promenljive) i reinicijalizaciju (dodeljivanje nove vrednosti).
 
 // let
 let y = 30;
@@ -18,7 +19,8 @@ console.log(y);
 
 // let y;
 
-// Za sve promenljive definisane preko let keyword je moguce izvrsiti reinicijalizaciju (dodeljivanje nove vrednosti), ali, kada govorimo o istom prostoru (scope) nece biti moguca redeklaracija (novo obezbedjivanje memorije u prostoru).
+// Za sve promenljive definisane preko let keyword je moguce izvrsiti reinicijalizaciju (dodeljivanje nove vrednosti), ali,
+// kada govorimo o istom prostoru (scope) nece biti moguca redeklaracija (novo obezbedjivanje memorije u prostoru).
 
 // let i const keyword nam obezbedjuju block scope (dodatni prostor za definisanje promenljivih).
 // Promenljive definisane preko let ili const keyword su block scope promenljive.
@@ -50,7 +52,8 @@ console.log(w);
   console.log(w);
 }
 
-// Sve karakteristike koje vaze za promenljive definisane preko let, vaze i za promenljive definisane preko const keyword, kada govorimo o block scope.
+// Sve karakteristike koje vaze za promenljive definisane preko let, vaze i za promenljive definisane preko
+// const keyword, kada govorimo o block scope.
 
 const niz = [12, 13, 15, 16];
 // niz = ["bilo", "sta"];
