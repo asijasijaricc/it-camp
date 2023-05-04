196edf67608453751023511042f1fec6f920522b
asijasijaricc <asijasijaricc12@gmail.com>
2023-05-04 21:08:21 +0200

    domaci

---
+++ Domaci JavaScript/domaci 7/app.js
@@ -0,0 +1,39 @@
+// 1. Napraviti novu recenicu gde ce umesto slova "a" pisati "t".
+// Dok ako se posle slova "a" nalazi slovo "n" pisace "d" umesto "a".
+
+// 2. Na osnovu date recenice napraviti i ispisati novu recenicu tako da:
+
+// (i)	se svi samoglasnici pretvore u velika slova;
+// (ii)	ostali karakteri se ne menjaju.
+
+// 1.
+
+// const recenica1 = prompt("Unesite neku recenicu:");
+// let recenica2 = "";
+
+// for (let i = 0; i < recenica1.length; i++) {
+//   if (recenica1[i] === "a") {
+//     console.log("t");
+//   } else if (recenica1[i] === "")
+// }
+
+// 2.
+
+// const recenica3 = prompt("Unesite neku recenicu:");
+// let recenica4 = " ";
+
+// for (let i = 0; i < recenica3.length; i++) {
+//   if (
+//     recenica3[i] === "a" ||
+//     recenica3[i] === "e" ||
+//     recenica3[i] === "i" ||
+//     recenica3[i] === "o" ||
+//     recenica3[i] === "u"
+//   ) {
+//     recenica3[i] += recenica4.toUpperCase;
+//   } else {
+//     recenica3[i] += recenica4;
+//   }
+// }
+
+// console.log(recenica4);
