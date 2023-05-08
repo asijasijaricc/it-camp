3fb7a9ac0c3145e62c26108824532ddb106db4cf
asijasijaricc <asijasijaricc12@gmail.com>
2023-05-08 17:15:03 +0200

    domaci

---
+++ Domaci JavaScript/domaci 8/app.js
@@ -0,0 +1,14 @@
+// Domaci zadaci:
+// 1. Napraviti funkciju koja vraca zbir kvadrata dva broja. Ako se izostavi drugi argument, funkcija ga racuna kao 0.
+
+// 2. Napraviti funkciju koja vraca aritmeticku sredinu 3 broja. Sa defaultnim vrednostima.
+
+// 3. Napraviti funkciju povrsina koja uzima dve vrednosti.
+// Ako su te dve vrednosti jednake da vraca povrsinu kvadrata uz odredjenu poruku,
+// dok ako su razlicite vrednosti da vraca povrsinu pravougaonika uz odredjenu poruku
+
+// 2.
+
+function aritmetickaSredina(br1, br2, br3) {
+  return br1 + br2 + br3;
+}
