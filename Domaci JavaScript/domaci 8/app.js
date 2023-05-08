4b4c9f701d6bb2bf24acb8d340cde690f81ea776
asijasijaricc <asijasijaricc12@gmail.com>
2023-05-08 17:42:41 +0200

    domaci

--- Domaci JavaScript/domaci 8/app.js
+++ Domaci JavaScript/domaci 8/app.js
@@ -7,8 +7,34 @@
 // Ako su te dve vrednosti jednake da vraca povrsinu kvadrata uz odredjenu poruku,
 // dok ako su razlicite vrednosti da vraca povrsinu pravougaonika uz odredjenu poruku
 
+// 1.
+
+function zbirKvadrata(dr1, dr2) {
+  if (dr2 === undefined) {
+    return dr2 === 0;
+  } else {
+    return dr1 * dr1 + dr2 * dr2;
+  }
+}
+
+console.log(zbirKvadrata(2));
+
 // 2.
 
 function aritmetickaSredina(br1, br2, br3) {
-  return br1 + br2 + br3;
+  return (br1 + br2 + br3) / 3;
 }
+
+console.log(aritmetickaSredina(2, 3, 4));
+
+// 3.
+
+function povrsina(vr1, vr2) {
+  if (vr1 === vr2) {
+    return vr1 * 2;
+  } else {
+    return vr1 * vr2;
+  }
+}
+
+console.log(povrsina(2, 2));
