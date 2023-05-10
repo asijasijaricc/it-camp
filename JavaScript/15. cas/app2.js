0431935f6e6736e6e67f8f75ab3a79944ef28674
asijasijaricc <asijasijaricc12@gmail.com>
2023-05-10 21:18:58 +0200

    terminal

---
+++ JavaScript/15. cas/app2.js
@@ -0,0 +1,30 @@
+// STRINGOVI //
+// Stringovi su immutable vrednosti.
+// Stringovi su primitivni (osnovni) tip podataka.
+
+// Za dobijanje duzine stringa koristimo length metodu:
+let a = "string";
+duzina = a.length;
+
+// String mozemo zapisati na nekoliko nacina:
+
+// 1. Koristeci duple navodnike:
+("Danas je bilo promenljivo vreme.");
+
+// 2. Koristeci obicne navodnike:
+// 'Danas je bilo promenljivo vreme.'
+
+// 3. Tamplate riterals (back-tic sintaksa)
+`Danas je bilo promenljivo vreme.`;
+
+// Dupli navodnici:
+
+("Danas je bilo 'promeljivo' vreme");
+
+// ("Danas je bilo \"promeljivo\" vreme");
+
+// Obicni navodnic:
+
+('Danas je bilo "promeljivo" vreme');
+
+// ('Danas je bilo \'promeljivo\' vreme');
