2f224bdd4bf870629832a6af390d90b7bbd21b27
asijasijaricc <asijasijaricc12@gmail.com>
2023-05-17 13:45:46 +0200

    domaci

---
+++ Domaci JavaScript/domaci 10/app.js
@@ -0,0 +1,16 @@
+// Domaci:
+// Zadatak: Spajanje stringova
+// Napiši funkciju koja prima dva stringa kao argumenta i vraća njihovu konkatenaciju.
+// Na primer, za ulazne vrednosti "Hello" i "World!", funkcija treba da vrati "HelloWorld!".
+
+// Zadatak: Palindrom
+// Napiši funkciju koja prima jedan string kao argument i proverava da li je taj string palindrom,
+// tj. da li se čita isto i s leva na desno i s desna na levo.
+// Funkcija treba da vrati true ako je string palindrom, inače false.
+// Na primer, za ulazni string "radar" funkcija treba da vrati true, dok za ulazni string "hello" funkcija treba da vrati false.
+
+function spajanjeStringa(string1, string2) {
+  string1 += string2;
+}
+
+console.log(spajanjeStringa("hello", "world"));
