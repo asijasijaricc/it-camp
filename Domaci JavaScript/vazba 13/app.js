4e7e4fb568bf0f79300eb386703ba0460c16143b
asija <123770204+asijasijaricc@users.noreply.github.com>
2023-05-24 19:24:10 +0200

    domaci

---
+++ Domaci JavaScript/vazba 13/app.js
@@ -0,0 +1,37 @@
+// Domaci zadaci:
+// 1. Napraviti funkciju koja ispituje argument.
+// Ako se u recenici nadje rec "skola", neka se vrati pozicija prvog pojavljivanja date reci.
+// Ako recenica pocinje sa recju "Automobil" neka se vrati boolean true;
+// Ako je recenica parne duzine neka se vrati broj pojavljivanja reci "kuca".
+// Inace vratiti duzinu datog stinga.
+
+// 2. Prebrojati koliko ima malih slova u unetom stringu. Slova koja se koriste u stringu pripadaju isključivo engleskoj abecedi.
+
+// 1.
+
+function prvaFunc(recenica) {
+  if (recenica.includes("skola")) {
+    return `Pozicija prvog ponavljanja je: ${recenica.indexOf("skola")}`;
+  } else if (recenica.startsWith("Autmobil")) {
+    return true;
+  } else if (recenica.length % 2 === 0) {
+    return recenica.split("kuca").length - 1;
+  } else {
+    recenica.length;
+  }
+}
+
+console.log(prvaFunc("kuca kuca kuca"));
+
+// 2.
+
+function malaSlova(recenica1) {
+  let brojac = 0;
+  for (var i = 0; i < string.length; i++) {
+    let slovo = recenica1[i];
+    if (slovo >= "a" && slovo <= "z") {
+      brojac++;
+    }
+  }
+  return brojac;
+}
