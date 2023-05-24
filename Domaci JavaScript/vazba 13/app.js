da5a927da35afbef004cfa2106236c5d7a559645
asija <123770204+asijasijaricc@users.noreply.github.com>
2023-05-24 21:07:44 +0200

    zadatak

--- Domaci JavaScript/vazba 13/app.js
+++ Domaci JavaScript/vazba 13/app.js
@@ -7,27 +7,50 @@
 
 // 2. Prebrojati koliko ima malih slova u unetom stringu. Slova koja se koriste u stringu pripadaju isključivo engleskoj abecedi.
 
-// 1.
+// 1. prvi nacin
 
 function prvaFunc(recenica) {
   if (recenica.includes("skola")) {
     return `Pozicija prvog ponavljanja je: ${recenica.indexOf("skola")}`;
-  } else if (recenica.startsWith("Autmobil")) {
+  } else if (recenica.startsWith("Automobil")) {
     return true;
   } else if (recenica.length % 2 === 0) {
     return recenica.split("kuca").length - 1;
   } else {
-    recenica.length;
+    return recenica.length;
   }
 }
 
-// console.log(prvaFunc("kuca kuca kuca"));
+console.log(prvaFunc("Automobil je crven"));
 
-// 2.
+// 1. drugi nacin
+
+const first = (recenica) => {
+  if (recenica.includes("skola")) {
+    return recenica.indexOf("skola");
+  } else if (recenica.startsWith("Automobil")) {
+    return true;
+  } else if (recenica.length % 2 === 0) {
+    if (recenica.match(/kuca/g) === null) {
+      return 0;
+    } else {
+      return recenica.match(/kuca/g).length;
+    }
+  } else {
+    return recenica.length;
+  }
+};
+
+console.log(first("Koja se skola podrazumeva ?"));
+console.log(first("Automobil na prodaju"));
+console.log("Parna recenica kuca ");
+console.log("parna recenica kuca");
+
+// 2. prvi nacin
 
 function malaSlova(recenica1) {
   let brojac = 0;
-  for (var i = 0; i < string.length; i++) {
+  for (let i = 0; i < recenica1.length; i++) {
     let slovo = recenica1[i];
     if (slovo >= "a" && slovo <= "z") {
       brojac++;
@@ -35,3 +58,33 @@ function malaSlova(recenica1) {
   }
   return brojac;
 }
+
+console.log(malaSlova("Neki string recenica"));
+
+// 2. drugi nacin
+
+const second = (recenica) => {
+  let brojac = 0;
+  for (let i = 0; i < recenica.length; i++) {
+    if (recenica[i] >= "a" && recenica[i] <= "z") {
+      brojac++;
+    }
+  }
+  return brojac;
+};
+
+console.log(second("Koliko ima MALIH SLOVA?"));
+
+console.log("a");
+console.log("b");
+
+console.log("a" > "b");
+console.log("a" < "b");
+
+console.log("a" > "A");
+
+// Prvo idu velika slova, pa onda mala.
+console.log("a".charCodeAt(0));
+console.log("Z".charCodeAt(0));
+console.log("A".charCodeAt(0));
+console.log("z".charCodeAt(0));
---
+++ JavaScript/21. cas/app.js
@@ -0,0 +1,48 @@
+var b = 7;
+
+let a = 4;
+
+const c = 9;
+
+// Cesto u literaturi cemo naici na tvrdnju da je JavaScript interpreterski jezik, ali to nije bas tako.
+// U pozadini postoji JS masina koja vrsi pozadinsko kompajliranje pre izvrsavanja koda.
+// Stoga nije pogresno reci da je JS kompajliran jezik.
+
+// Pojam hoisting u JavaScriptu predstavlja izvlacenje svih deklaracija na vrhu skripte (fajla).
+
+d = 11;
+console.log(d);
+
+// Kasnije deklariranje neke promenljive je moguce preko var keyword, dok preko let i const nije dozvoljeno.
+
+var d;
+// let d;
+// const d;
+
+// 1. Ispitati da li u unetom stringu ima vise malih ili velikih slova.
+// Slova koja se koriste u stringu pripadaju iskljucivo engleskoj abecedi.
+
+function kojihJeVise(recenica) {
+  let brVelikih = 0;
+  let brMalih = 0;
+  for (let i = 0; i < recenica.length; i++) {
+    if (recenica[i] >= "a" && recenica[i] <= "z") {
+      brMalih++;
+    } else if (recenica[i] >= "A" && recenica[i] <= "Z") {
+      brVelikih++;
+    }
+  }
+  if (brVelikih > brMalih) {
+    return `U datoj recenici ima vise velikih slova. Ima ih ${brVelikih}, dok malih ima ${brMalih}`;
+  } else if (brMalih > brVelikih) {
+    return `U datoj recenici ima vise malih slova. Ima ih ${brMalih}, dok malih ima ${brVelikih}`;
+  } else {
+    return `U datoj recenici ima jednak broj velikih i malih slova, ${brMalih}.`;
+  }
+}
+
+console.log(kojihJeVise("Treba imati VISE malih slova."));
+
+console.log(kojihJeVise("Treba imati VISE velikih slova."));
+
+console.log(kojihJeVise("mala i VELIKA s"));
