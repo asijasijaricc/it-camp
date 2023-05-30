fa01478339bb842e3b0307a697b4926a7bbf7d87
asija <123770204+asijasijaricc@users.noreply.github.com>
2023-05-30 22:27:09 +0200

    domaci

--- Domaci JavaScript/domaci 15/app.js
+++ Domaci JavaScript/domaci 15/app.js
@@ -1,4 +1,14 @@
 // 1.	Izdvojiti N znakova sa desne strane (poslednjih N znakova) unetog stringa.
 // Npr. za string 'Pera ima devojku' i N=5, dobija se 'vojku'.
 
-function Nznak(recenica) {}
+function Nznak(string, N) {
+  if (N >= string.length) {
+    return string;
+  } else {
+    return string.slice(-N);
+  }
+}
+let string2 = "Pera ima devojku";
+let N = 5;
+let nekiZnak = Nznak(string2, N);
+console.log(nekiZnak);
---
+++ Domaci JavaScript/domaci 16/app.js
@@ -0,0 +1,4 @@
+// Domaci:
+// 1. Write a JavaScript function to hide email addresses to protect from unauthorized user.
+// console.log(protectEmail("robin_singh@example.com")); (posle imena moze biti ili _ ili .)
+// "robin...@example.com"
