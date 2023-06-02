085f3a24f3aa2beac282f0186c24c6f98082b86f
asija <123770204+asijasijaricc@users.noreply.github.com>
2023-06-02 20:53:18 +0200

    brojevi

--- JavaScript/25. cas/app2.js
+++ JavaScript/25. cas/app2.js
@@ -25,3 +25,40 @@ console.log(typeof +broj);
 let broj2 = z.toPrecision(4);
 console.log(broj2);
 console.log(typeof broj2);
+
+// Bolji u dve discipline:
+
+// function bolji(mat1, pro1, mat2, pro2) {
+//   if (mat1 > mat2 && pro1 > pro2) {
+//     return "Pobednik je prvi takmicar";
+//   } else if (mat2 > mat1 && pro2 > pro1) {
+//     return "Pobednik je drugi takmicar";
+//   } else if (mat1 === mat2 && pro1 > pro2) {
+//     return "Pobednik je prvi takmicar";
+//   } else if (mat1 === mat2 && pro1 < pro2) {
+//     return "Pobednik je drugi takmicar";
+//   }
+// }
+
+// console.log(bolji(16, 17, 16, 15));
+
+function bolji(mat1, pro1, mat2, pro2) {
+  let poeni1 = mat1 + pro1;
+  let poeni2 = mat2 + pro2;
+  if (poeni1 < poeni2) {
+    return "Pobedio je drugi takmicar.";
+  } else if (poeni1 === poeni2) {
+    if (pro1 < pro2) {
+      return "Pobedio je drugi takmicar";
+    } else {
+      return "Pobedio je prvi takmicar";
+    }
+  } else {
+    return "Pobedio je prvi takmicar";
+  }
+}
+
+console.log(bolji(34, 40, 45, 45));
+console.log(bolji(50, 50, 45, 45));
+console.log(bolji(50, 50, 50, 50));
+console.log(bolji(23, 32, 32, 23));
