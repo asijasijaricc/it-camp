ecf1d83428de4e5245ede0e8c659635c75a57c7b
asijasijaricc <asijasijaricc12@gmail.com>
2023-06-09 19:42:45 +0200

    domaci

---
+++ Domaci JavaScript/domaci 19/app.js
@@ -0,0 +1,6 @@
+// Domaci zadaci:
+// 1. Napraviti funkciju koja ocekuje cetvorocifren broj, a vraca broj kojem su zamenjene prva i treca cifra, kao i druga i cetvrta cifra.
+// 4752
+// 5247
+
+// 2. Write a JavaScript program to find a value which is nearest to 100 from two different given integer values.
