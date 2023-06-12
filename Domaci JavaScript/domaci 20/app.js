08679f9251e0243b6525b6d7a3ee9c2f5fb28a01
asijasijaricc <asijasijaricc12@gmail.com>
2023-06-12 17:50:22 +0200

    domaci

--- Domaci JavaScript/domaci 20/app.js
+++ Domaci JavaScript/domaci 20/app.js
@@ -10,13 +10,13 @@ function range(num1, num2) {
   if ((num1 >= 40 && num1 <= 60) || (num2 >= 40 && num2 <= 60)) {
     return `Number is in the range between 40 and 60`;
   } else if ((num1 >= 70 && num1 <= 100) || (num2 >= 70 && num2 <= 100)) {
-    return `Number is in the range between 40 and 60`;
+    return `Number is in the range between 70 and 100`;
   } else {
     ("Numbers arent in the ranges.");
   }
 }
 
-console.log(range(20, 10));
+console.log(range(45, 70));
 
 // 2.
 
@@ -31,4 +31,4 @@ function larger(num1, num2) {
     return "Number are not in the range";
   }
 }
-console.log(larger(45, 42));
+console.log(larger(47, 42));
