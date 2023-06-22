ef4b08cde14b830466a06be41ed82afc9ee2aa8d
asijasijaricc <asijasijaricc12@gmail.com>
2023-06-22 22:43:16 +0200

    domaci

--- Domaci JavaScript/domaci 25/app.js
+++ Domaci JavaScript/domaci 25/app.js
@@ -3,17 +3,6 @@
 // koliko tipa number,
 // koliko tipa boolean,
 // koliko elemenata je zapravo niz?
-const nizz = [
-  "jagoda",
-  25,
-  true,
-  "mandarina",
-  [1, 2, 3],
-  "ananas",
-  49,
-  694,
-  "tresnja",
-];
 
 function tip(niz) {
   let stringovi = 0;
