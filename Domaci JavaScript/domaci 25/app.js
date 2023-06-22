21a16d2a0225e65b88f0889505cc650cd2e08dcc
asijasijaricc <asijasijaricc12@gmail.com>
2023-06-22 22:38:40 +0200

    domaci

---
+++ Domaci JavaScript/domaci 25/app.js
@@ -0,0 +1,58 @@
+// Domaci:
+// Iz datog niza ispitati koliko elemenata je tipa string,
+// koliko tipa number,
+// koliko tipa boolean,
+// koliko elemenata je zapravo niz?
+const nizz = [
+  "jagoda",
+  25,
+  true,
+  "mandarina",
+  [1, 2, 3],
+  "ananas",
+  49,
+  694,
+  "tresnja",
+];
+
+function tip(niz) {
+  let stringovi = 0;
+  let brojevi = 0;
+  let boolean = 0;
+  let nizici = 0;
+  let nizovi = 0;
+
+  for (let i = 0; i < niz.length; i++) {
+    if (typeof niz[i] === "string") {
+      stringovi++;
+    } else if (typeof niz[i] === "number") {
+      brojevi++;
+    } else if (typeof niz[i] === "boolean") {
+      boolean++;
+    } else if (typeof niz[i] === Array) {
+      nizici++;
+    }
+    if (Array.isArray(niz)) {
+      nizovi++;
+    }
+  }
+  return `Broj string elemenata je ${stringovi}.
+  Broj number elemenata je ${brojevi}.
+  Broj boolean elemenata je ${boolean}.
+  Broj array elemenata je ${nizici}
+  Broj elemenata unutar niza je ${nizovi}`;
+}
+
+console.log(
+  tip([
+    "jagoda",
+    25,
+    true,
+    "mandarina",
+    [1, 2, 3],
+    "ananas",
+    49,
+    694,
+    "tresnja",
+  ])
+);
