0e8956c07e0fff9755a0339d8b2d07c099a6f25a
asijasijaricc <asijasijaricc12@gmail.com>
2023-07-05 19:03:24 +0200

    vezba

---
+++ Domaci JavaScript/vezba 3/app.js
@@ -0,0 +1,7 @@
+// Kreirati funkciju koja radi sledece:
+// Koristeci prompt uneti 5 imena radnika, potom od njih napraviti niz i izvrsiti sledece:
+// Najpre od postojecih imena kreirati email adrese, tako sto cete na ime dodati "@gmail.com", i
+// te nove adrese smestiti u novi niz, a potom napraviti da se na svaki klik na dugme "Prikazi"
+// prikazuje alert-a u kome je ispisana neka od adresa u nizu. Bitno: na svaki klik treba da izlazi
+// drugacija adresa, to uraditi koristeci random generisanje.
+// Funkcija se poziva odmah pri pokretanju programa.
