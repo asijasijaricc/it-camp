723d028d3987c85def1edb4b1afb2bf5c2b8fd8b
asijasijaricc <asijasijaricc12@gmail.com>
2023-07-12 18:30:30 +0200

    domaci

---
+++ Domaci JavaScript/domaci 30/app.js
@@ -0,0 +1,19 @@
+// Domaci:
+// 1. From the array of numbers, choose even double even numbers and compute the sum using Array's filter, map and reduce methods.
+
+// 2. Write a JavaScript program which accept a string as input and swap the case of each character.
+// For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'.
+
+// 1.
+
+function even(arr) {
+  const arr1 = arr.filter((num) => {
+    num % 2 === 0;
+  });
+
+  arr1.reduce((prev, curr) => {
+    prev + curr;
+  });
+}
+
+console.log(even(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
