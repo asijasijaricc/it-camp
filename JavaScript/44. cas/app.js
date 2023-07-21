b32fe1159536c0b6b5130e53ae389f8d370151b0
asijasijaricc <asijasijaricc12@gmail.com>
2023-07-21 20:04:38 +0200

    JSON

---
+++ JavaScript/44. cas/app.js
@@ -0,0 +1,10 @@
+// JSON JavaScript Object Nation //
+
+// JSON je format za skladistenje i prenos podataka. Najcesce se koristi kada se podaci salju sa backenda ka WEB stranici.
+// Sintaksa je vrlo slicna JavaScript objektima:
+
+// const person = {
+//     "firstName": "Asija",
+//     "lastName": "Sijaric",
+//     "age": 19
+// }
