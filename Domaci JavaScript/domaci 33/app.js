079cdae538edb9786c7b25a05494ad67c05c101f
asijasijaricc <asijasijaricc12@gmail.com>
2023-07-23 18:25:01 +0200

    domaci

---
+++ Domaci JavaScript/domaci 33/app.js
@@ -0,0 +1,22 @@
+// Domaci:
+// Napraviti objekat sa sledecim svojstvima:
+// 1. firstName,
+// 2. lastName,
+// 3. language,
+// 4. setLanguage - metoda za setovanje jezika,
+// 5. setNickName - metoda (svaka osoba ima nadimak sastavljen od prva dva slova
+// imena i prva dva slova prezimena)
+
+let person = {
+  firstName: "Asija",
+  lastName: "Sijaric",
+  lenguage: ["Serbian", "English"],
+};
+
+person.setLenguage = function (jezik) {
+  this.jezik += lenguage;
+};
+
+console.log(person.setLenguage("German"));
+
+person;
