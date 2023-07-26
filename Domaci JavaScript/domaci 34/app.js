76db718d79ec2a6186407c662ca1a15e28bd1ab2
asijasijaricc <asijasijaricc12@gmail.com>
2023-07-26 14:05:51 +0200

    domaci

---
+++ Domaci JavaScript/domaci 34/app.js
@@ -0,0 +1,40 @@
+// Domaci:
+// const automobil = {
+//     marka: "Audi",
+//     model: "Q7",
+//     boja: "Bela",
+//     pogon: "quattro",
+//     menjac: "Automatik",
+//     km:240000,
+//     vlasnik:["062321552","063930630"],
+//     garaza:{
+//         parking:"JKP Servis",
+//         vikend:"od 17h free",
+//         satnaKarta: "50",
+//         dnevnaKarta: "200",
+//         mesecnaKarta: "2000",
+//         platiZa: function(od, do) {
+//
+// }
+//     }
+// }
+
+//  2. Write a JavaScript program to sort an array of JavaScript objects:
+
+const library = [
+  {
+    title: "The Road Ahead",
+    author: "Bill Gates",
+    libraryID: 1254,
+  },
+  {
+    title: "Walter Isaacson",
+    author: "Steve Jobs",
+    libraryID: 4264,
+  },
+  {
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    author: "Suzanne Collins",
+    libraryID: 3245,
+  },
+];
