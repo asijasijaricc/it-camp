fecc7d8b6b835f289de4e1a30e8be59578c3721b
asijasijaricc <asijasijaricc12@gmail.com>
2023-07-27 00:38:23 +0200

    domaci koji ne znam da uradim

---
+++ Domaci JavaScript/domaci 35/app.js
@@ -0,0 +1,51 @@
+// Domaci:
+// 1. Write a JavaScript function to retrieve all the values of an object's properties.
+
+// 2. Napraviti niz objekata, gde ce svaki objekat sadrzati:
+// ime, prezime, starost, bojaOciju, bojaKose, polozenC.
+//  Napraviti funkciju koja pravi novi niz koji ce sadrzati one elemente cija boja ociju je braon i koji nisu polozili C.
+
+const arr = [
+  {
+    ime: "Anastasija",
+    prezime: "Milovic",
+    starost: 17,
+    bojaOciju: "braon",
+    polozenC: false,
+  },
+  {
+    ime: "Merjem",
+    prezime: "Sinanovic",
+    starost: 17,
+    bojaOciju: "plava",
+    polozenC: true,
+  },
+  {
+    ime: "Emir",
+    prezime: "Marukic",
+    starost: 22,
+    bojaOciju: "braon",
+    polozenC: true,
+  },
+  {
+    ime: "Ajsa",
+    prezime: "Medjedovic",
+    starost: 18,
+    bojaOciju: "braon",
+    polozenC: false,
+  },
+  {
+    ime: "Ajla",
+    prezime: "Lomnicanin",
+    starost: 22,
+    bojaOciju: "braon",
+    polozenC: true,
+  },
+  {
+    ime: "Asija",
+    prezime: "Sijaric",
+    starost: 19,
+    bojaOciju: "plava",
+    polozenC: false,
+  },
+];
