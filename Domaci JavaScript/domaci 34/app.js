6227521a159cfbc46ce7bccf0bb3413df9513d11
asijasijaricc <asijasijaricc12@gmail.com>
2023-07-26 19:06:01 +0200

    domaci

--- Domaci JavaScript/domaci 34/app.js
+++ Domaci JavaScript/domaci 34/app.js
@@ -1,23 +1,33 @@
 // Domaci:
-// const automobil = {
-//     marka: "Audi",
-//     model: "Q7",
-//     boja: "Bela",
-//     pogon: "quattro",
-//     menjac: "Automatik",
-//     km:240000,
-//     vlasnik:["062321552","063930630"],
-//     garaza:{
-//         parking:"JKP Servis",
-//         vikend:"od 17h free",
-//         satnaKarta: "50",
-//         dnevnaKarta: "200",
-//         mesecnaKarta: "2000",
-//         platiZa: function(od, do) {
-//
-// }
-//     }
-// }
+const automobil = {
+  marka: "Audi",
+  model: "Q7",
+  boja: "Bela",
+  pogon: "quattro",
+  menjac: "Automatik",
+  km: 240000,
+  vlasnik: ["062321552", "063930630"],
+  garaza: {
+    parking: "JKP Servis",
+    vikend: "od 17h free",
+    satnaKarta: "50",
+    dnevnaKarta: "200",
+    mesecnaKarta: "2000",
+    platiZa: function (datum1, datum2) {
+      const dan = 24 * 60 * 6 * 1000;
+      const od = new Date(datum1);
+      const doo = new Date(datum2);
+      const dani = Math.round((doo - od) / dan);
+      const ukupno = this.garaza.dnevnaKarta * dani;
+      return ukupno;
+    },
+  },
+};
+
+const od = "2023-12-20";
+const doo = "2023-12-27";
+const iznos = automobil.garaza.platiZa(od, doo);
+console.log("Ukupan iznos za parking:", iznos);
 
 //  2. Write a JavaScript program to sort an array of JavaScript objects:
 
@@ -38,3 +48,5 @@ const library = [
     libraryID: 3245,
   },
 ];
+
+function covert(a, b) {}
