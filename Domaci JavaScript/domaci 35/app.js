7efe4f1b676a08aba272445a11263e723415a662
asijasijaricc <asijasijaricc12@gmail.com>
2023-07-28 20:12:20 +0200

    domaci

--- Domaci JavaScript/domaci 35/app.js
+++ Domaci JavaScript/domaci 35/app.js
@@ -1,6 +1,42 @@
 // Domaci:
 // 1. Write a JavaScript function to retrieve all the values of an object's properties.
 
+const itCamp = {
+  duzina: "9 meseci",
+  brojTehnologija: 4,
+  prvaTehnologija: "HTML",
+  drugaTehnologija: "CSS",
+  trecaTehnologija: "JavaScript",
+  cetvrtaTehnologija: "React",
+  ocekivaniIshod: "Junior Frontend Developer",
+};
+
+// for in
+
+// for (let x in itCamp) {
+//   console.log(x); // x je key za svaki property objekta
+//   console.log(itCamp[x]); // ispisuje se value za svaki property objekata
+
+// }
+
+// 1. nacin
+
+const values = (obj) => {
+  const values = [];
+  for (let key in obj) {
+    values.push(obj[key]);
+  }
+  return values;
+};
+
+console.log(values(itCamp));
+
+// 2.
+
+const newarr = Object.values(itCamp);
+
+console.log(newarr);
+
 // 2. Napraviti niz objekata, gde ce svaki objekat sadrzati:
 // ime, prezime, starost, bojaOciju, bojaKose, polozenC.
 //  Napraviti funkciju koja pravi novi niz koji ce sadrzati one elemente cija boja ociju je braon i koji nisu polozili C.
@@ -49,3 +85,12 @@ const arr = [
     polozenC: false,
   },
 ];
+
+const filterStudents = (arr) => {
+  return arr.filter(
+    // (student) => student.bojaOciju === "braon" && student.polozenC === false
+    (student) => student.bojaOciju === "braon" && !student.polozenC
+  );
+};
+
+console.log(filterStudents(arr));
