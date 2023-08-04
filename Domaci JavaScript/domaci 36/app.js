0676c0f0f60928a6af9703012fcb8c13b091685c
asijasijaricc <asijasijaricc12@gmail.com>
2023-08-04 15:20:19 +0200

    domaci

---
+++ Domaci JavaScript/domaci 36/app.js
@@ -0,0 +1,4 @@
+// Domaci:
+// 1.  Iz 2 poslata niza vratiti 3. niz koji ce sadrzati one elemente iz oba niza koji nisu sadrzani u onom drugom nizu.
+const niz1 = [1, 3, 4, 5, 6, 7, 9];
+const niz2 = [2, 4, 5, 6, 8, 10];
