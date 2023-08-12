621542c91078e27b5d3af1411c4b0f361b2ee7e9
asijasijaricc <asijasijaricc12@gmail.com>
2023-08-12 15:18:11 +0200

    domaci

--- Domaci JavaScript/domaci 39/app.js
+++ Domaci JavaScript/domaci 39/app.js
@@ -3,4 +3,28 @@
 // ako argument ima manje od 11 samoglasnika neka se reject_a obecanje uz poruku "String ne zadovoljava uslov";
 // U svakom slucaju neka stoji poruka "Funkcija je izvrsena".
 
-const smoglas = new Promise((resolve, reject) => {});
+const samoglas = new Promise((resolve, reject) => {
+  const str = "Neki string koji ima mnogo samoglasnika";
+  let brojac = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] === "a" ||
+      str[i] === "e" ||
+      str[i] === "i" ||
+      str[i] === "o" ||
+      str[i] === "u"
+    ) {
+      brojac++;
+    }
+  }
+  if (brojac > 10) {
+    resolve("String zadovoljava uslov");
+  } else {
+    reject("String ne zadovoljava uslov");
+  }
+});
+
+samoglas
+  .then((mess) => console.log(mess))
+  .catch((messg) => console.log(messg))
+  .finally(() => console.log("Funkcija je izvrsena"));
