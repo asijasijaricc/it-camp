e2f8ad69295046a17771569172e5745ff75dc2db
asijasijaricc <asijasijaricc12@gmail.com>
2023-08-12 15:07:34 +0200

    domaci

---
+++ Domaci JavaScript/domaci 39/app.js
@@ -0,0 +1,6 @@
+// Napraviti funkciju koja sadrzi Promise koji proverava da li poslati argument funkcije ima vise od 10 samoglasnika.
+// Ako argument ima vise od 10 samoglasnika neka se uspesno izvrsi obecanje sa porukom "String zadovoljava uslov";
+// ako argument ima manje od 11 samoglasnika neka se reject_a obecanje uz poruku "String ne zadovoljava uslov";
+// U svakom slucaju neka stoji poruka "Funkcija je izvrsena".
+
+const smoglas = new Promise((resolve, reject) => {});
