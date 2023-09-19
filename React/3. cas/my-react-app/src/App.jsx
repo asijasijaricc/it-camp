f80c45de3df5586a04d37fd73d5d4e4e097da86c
asijasijaricc <asijasijaricc12@gmail.com>
2023-09-19 21:00:32 +0200

    3. cas

---
+++ React/3. cas/my-react-app/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div style={{ display: "flex" }} className="cel">
+        <div className="levi">
+          <h1>Kickstart Actually Pinterest Brunch Bitters Occupy </h1>
+        </div>
+        <div className="desni">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic,
+            eligendi odio repellat quis quia earum libero blanditiis dicta
+            molestias animi!
+          </p>
+          <button>Button</button>
+          <a href="">Learn More</a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
