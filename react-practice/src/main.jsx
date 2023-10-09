ef7229449d0794256f66991c0172c8f0581a8d12
asijasijaricc <asijasijaricc12@gmail.com>
2023-10-09 19:59:17 +0200

    10 cas

--- react-practice/src/main.jsx
+++ react-practice/src/main.jsx
@@ -1,31 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import NewCard from "./components/9. cas/newCard";
+import Class11 from "./components/11. cas";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div style={{ display: "flex",flexDirection:"column" gap: " 20px" }}>
-      <NewCard
-        slika="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwStAG_fMs0-huIwtWljxrR1X1Td5qCRH_-mnrbe-x&s"
-        jedan="CATEGORY"
-        htag="The Catalyzer"
-        tekstic="Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat."
-        nekilink="Learn More"
-      />
-      <NewCard
-        slika="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwStAG_fMs0-huIwtWljxrR1X1Td5qCRH_-mnrbe-x&s"
-        jedan="CATEGORY"
-        htag="The Catalyzer"
-        tekstic="Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat."
-        nekilink="Learn More"
-      />
-      <NewCard
-        slika="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwStAG_fMs0-huIwtWljxrR1X1Td5qCRH_-mnrbe-x&s"
-        jedan="CATEGORY"
-        htag="The Catalyzer"
-        tekstic="Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat."
-        nekilink="Learn More"
-      />
-    </div>
+    <Class11 num="13123" text="asija" />
   </React.StrictMode>
 );
