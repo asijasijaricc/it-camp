b6e49d769785fdf774f8d0e2d4164073c6a298cd
asijasijaricc <asijasijaricc12@gmail.com>
2023-11-10 18:45:33 +0100

    aa

---
+++ react-practice/src/components/23. cas/index.jsx
@@ -0,0 +1,13 @@
+import SimpleClassComponent from "./simpleclass";
+import SimpleEffects from "./simpleeffexts";
+
+const Class23 = () => {
+  return (
+    <div>
+      <SimpleClassComponent />
+      <SimpleEffects />
+    </div>
+  );
+};
+
+export default Class23;
